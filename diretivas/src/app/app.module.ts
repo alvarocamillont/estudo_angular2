1776422d233abbec4590cac2c81c341ba23362c0
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-01-02 13:46:07 -0200

    #18: Angular CLI: Criando components, services: ng generate

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgifComponent
   ],
   imports: [
     BrowserModule
