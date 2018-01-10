d402f518c231b87267f4d070fdd8c44daed5657c
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-01-10 18:18:15 -0200

    Trocando Processador de css

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.scss']
+  styleUrls: ['./diretiva-ngif.component.css']
 })
 export class DiretivaNgifComponent implements OnInit {
 
--- diretivas/src/app/diretiva-ngswitch/diretiva-ngswitch.component.ts
+++ diretivas/src/app/diretiva-ngswitch/diretiva-ngswitch.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngswitch',
   templateUrl: './diretiva-ngswitch.component.html',
-  styleUrls: ['./diretiva-ngswitch.component.scss']
+  styleUrls: ['./diretiva-ngswitch.component.css']
 })
 export class DiretivaNgswitchComponent implements OnInit {
 
