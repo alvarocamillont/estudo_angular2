135d10088e6441b55edfdc719c5f815dca4e1d92
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-01-11 09:26:52 -0200

    Estudo sobre no ngFor

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { NgForComponent } from './ng-for/ng-for.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    DiretivaNgswitchComponent
+    DiretivaNgswitchComponent,
+    NgForComponent
   ],
   imports: [
     BrowserModule
