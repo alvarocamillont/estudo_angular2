14e04ff8440c21b6e9f063f396f37349db3c1eea
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-01-29 17:44:02 -0200

    Retirado provide desnecessário

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CursoModule } from './cursos/curso.module';
-import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 
 
@@ -17,7 +16,6 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     CriarCursoModule,
     CursoModule
   ],
-  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
