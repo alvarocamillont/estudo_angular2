a5e484d43884a9b0d976c20e8842efd82060a90a
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-01-30 18:18:17 -0200

    Criação do exemplo para PIPE

--- pipes/src/app/exemplo-pipe/exemplo-pipe.component.ts
+++ pipes/src/app/exemplo-pipe/exemplo-pipe.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExemploPipeComponent implements OnInit {
 
+  livro: any = {
+    titulo: 'Aprendendo Javascript',
+    rating: 4.5665,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'https://tinyurl.com/ybr97fuk'
+  };
+
   constructor() { }
 
   ngOnInit() {
