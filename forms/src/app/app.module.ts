bcb55b22aaae6bc3d867016c115ce10484450d43
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-03-19 14:37:19 -0300

    Reorganizando em módulos

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -3,23 +3,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlertModule } from 'ngx-bootstrap';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
-import { FormsModule } from '@angular/forms';
+import { TemplateFormModule } from './template-form/template-form.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AlertModule.forRoot(),
-    FormsModule
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ forms/src/app/template-form/template-form.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TemplateFormComponent } from './template-form.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [
+    TemplateFormComponent
+  ]
+})
+export class TemplateFormModule { }
