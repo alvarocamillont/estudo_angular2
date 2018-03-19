260c4d1280be6ee5fcef73e9d050bc922888f6bd
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-03-19 18:50:26 -0300

    Incluido css para validação de formulário

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Alvaro',
-    email: 'alvaro.teste@email.com'
+    nome: null,
+    email: null
   };
 
   constructor() { }
