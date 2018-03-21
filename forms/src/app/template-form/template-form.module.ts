95b816d27bdcbe1eb8f5217249c0567b26782bbd
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-03-21 14:02:50 -0300

    Criado componente para debug de informações de formulário

--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
 
 @NgModule({
   imports: [
@@ -9,7 +10,8 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   declarations: [
-    TemplateFormComponent
+    TemplateFormComponent,
+    FormDebugComponent
   ]
 })
 export class TemplateFormModule { }
