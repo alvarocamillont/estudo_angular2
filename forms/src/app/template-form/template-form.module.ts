7c8e30248c15257203c5e008b8cc5725d3cf5989
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-04-30 13:49:50 -0300

    Refatoração para pasta shared

--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -2,20 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
-import { FormDebugComponent } from '../form-debug/form-debug.component';
-import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule
   ],
   declarations: [
-    TemplateFormComponent,
-    FormDebugComponent,
-    CampoControlErroComponent
+    TemplateFormComponent
   ]
 })
 export class TemplateFormModule { }
