9e900ac12810fc93bf7b469bfdd9149aa1a8bef1
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-04-30 14:16:20 -0300

    Aula 93 resetando campo

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,31 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-data-form',
-  templateUrl: './data-form.component.html',
-  styleUrls: ['./data-form.component.css']
+  selector: "app-data-form",
+  templateUrl: "./data-form.component.html",
+  styleUrls: ["./data-form.component.css"]
 })
 export class DataFormComponent implements OnInit {
-
   formulario: FormGroup;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private http: HttpClient ) { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   ngOnInit() {
-   this.formulario = this.formBuilder.group({
-     nome: [null],
-     email: [null],
-   });
+    this.formulario = this.formBuilder.group({
+      nome: [null],
+      email: [null]
+    });
   }
 
   onSubmit() {
     console.log(this.formulario);
-    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
-    .subscribe( dados => console.log(dados));
+    this.http
+      .post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+      .subscribe(dados => {
+        console.log(dados);
+        // reseta o form
+        this.resetar();
+      },
+      (error: any) => alert('erro')
+    );
   }
 
+  resetar() {
+    this.formulario.reset();
+  }
 }
--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -1,19 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { DataFormComponent } from './data-form.component';
-import { SharedModule } from '../shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+import { DataFormComponent } from "./data-form.component";
+import { SharedModule } from "../shared/shared.module";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    SharedModule,
-    HttpClientModule
-  ],
-  declarations: [
-    DataFormComponent
-  ]
+  imports: [CommonModule, ReactiveFormsModule, SharedModule, HttpClientModule],
+  declarations: [DataFormComponent]
 })
-export class DataFormModule { }
+export class DataFormModule {}
