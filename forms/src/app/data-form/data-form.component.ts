529d5404e7bd889cb0c0ebfbd7fb298562c94a50
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-05-03 18:57:23 -0300

    Aula 94 - Validação

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
 import { HttpClient } from "@angular/common/http";
 
 @Component({
@@ -14,8 +14,8 @@ export class DataFormComponent implements OnInit {
 
   ngOnInit() {
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, [Validators.required, Validators.minLength(3) ]],
+      email: [null, [Validators.required, Validators.email] ]
     });
   }
 
