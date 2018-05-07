b7da222ac5c73f346fa9409dccbf742d1da70106
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2018-05-07 17:00:27 -0300

    aula 98 - agrupando por grupo

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -16,14 +16,15 @@ export class DataFormComponent implements OnInit {
     this.formulario = this.formBuilder.group({
       nome: [null, [Validators.required, Validators.minLength(3) ]],
       email: [null, [Validators.required, Validators.email] ],
-      cep: [null, [Validators.required ]],
-      numero: [null, [Validators.required ]],
-      complemento: [null],
-      rua: [null, [Validators.required ]],
-      bairro: [null, [Validators.required ]],
-      cidade: [null, [Validators.required ]],
-      estado: [null, [Validators.required ]]
-
+      endereco: this.formBuilder.group({
+        cep: [null, [Validators.required ]],
+        numero: [null, [Validators.required ]],
+        complemento: [null],
+        rua: [null, [Validators.required ]],
+        bairro: [null, [Validators.required ]],
+        cidade: [null, [Validators.required ]],
+        estado: [null, [Validators.required ]]
+      })
     });
   }
 
