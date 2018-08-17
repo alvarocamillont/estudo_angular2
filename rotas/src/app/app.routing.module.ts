4cd8026dab52bd45136c863af2014a7ba578c49f
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2018-08-17 17:18:06 -0300

    teste

--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ModuleWithProviders } from '@angular/core';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
