23b321c1a172b55b8600915f441d3c6ab5eae5ab
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2018-08-24 14:35:44 -0300

    Curso Angular #106: Formulários reativos: Checkbox Toggle

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -44,7 +44,8 @@ export class DataFormComponent implements OnInit {
       }),
       cargo: [null],
       tecnologias: [null],
-      newsletter: ['s']
+      newsletter: ['s'],
+      termos: [null, Validators.pattern('true')]
     });
   }
 
