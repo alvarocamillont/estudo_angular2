cd949706d30cfad510e83d7de298ba4fef6fbac7
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2018-08-29 15:12:36 -0300

    Curso Angular #110: Formulários reativos: Validação entre dois campos (confirmar email)

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -42,6 +42,7 @@ export class DataFormComponent implements OnInit {
     this.formulario = this.formBuilder.group({
       nome: [null, [Validators.required, Validators.minLength(3)]],
       email: [null, [Validators.required, Validators.email]],
+      confirmarEmail: [null, [FormValidations.equalsTo('email')]],
       endereco: this.formBuilder.group({
         cep: [null, [Validators.required, FormValidations.cepValidator]],
         numero: [null, [Validators.required]],
--- forms/src/app/shared/form-validation.ts
+++ forms/src/app/shared/form-validation.ts
@@ -1,4 +1,4 @@
-import { FormArray, FormControl } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 export class FormValidations {
   static requiredMinCheckBox(min = 1) {
@@ -22,5 +22,31 @@ export class FormValidations {
         : { cepInvalido: true };
     }
     return null;
+  }
+
+  static equalsTo(otherField: string) {
+    const validator = (formControl: FormControl) => {
+      if (otherField == null) {
+        throw new Error('É necessário informar o campo.');
+      }
+
+      if (!formControl.root || !(<FormGroup>formControl.root).controls) {
+        return null;
+      }
+
+      const field = (<FormGroup>formControl.root).get(otherField);
+
+      if (!field) {
+        throw new Error('É necessário um campo válido.');
+      }
+
+      if (field.value !== formControl.value) {
+        return { equalsTo: otherField };
+      }
+
+      return null;
+    };
+
+    return validator;
   }
 }
